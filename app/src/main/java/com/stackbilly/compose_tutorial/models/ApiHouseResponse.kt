1e16bfa34fcd45d46eac5d407c6c213862d4ee6a
Livingstone-Billy <livingstoneochadia@gmail.com>
2023-12-11 09:14:23 +0300

    changed structure of house api response

--- app/src/main/java/com/stackbilly/compose_tutorial/models/ApiHouseResponse.kt
+++ app/src/main/java/com/stackbilly/compose_tutorial/models/ApiHouseResponse.kt
@@ -3,12 +3,7 @@ package com.stackbilly.compose_tutorial.models
 import com.google.gson.annotations.SerializedName
 
 data class ApiHouseResponse(
-    @SerializedName("Description" ) var Description : String? = null,
-    @SerializedName("Image"       ) var Image       : String? = null,
-    @SerializedName("Location"    ) var Location    : String? = null,
-    @SerializedName("Name"        ) var Name        : String? = null,
-    @SerializedName("Price"       ) var Price       : String? = null,
-    @SerializedName("Url"         ) var Url         : String? = null,
-    @SerializedName("_id"         ) var Id          : String? = null
-
-)
+    @SerializedName("error" ) var error: String? = null,
+    @SerializedName("message") var message: String? = null,
+    @SerializedName("results"    ) var results: MutableList<Houses> = mutableListOf(),
+    )
