47bbef9c0a2cbf76481c05d2551a14fec4a6b80b
Livingstone-Billy <livingstoneochadia@gmail.com>
2023-12-11 09:15:04 +0300

    changed structure of house data class

--- app/src/main/java/com/stackbilly/compose_tutorial/models/Houses.kt
+++ app/src/main/java/com/stackbilly/compose_tutorial/models/Houses.kt
@@ -1,10 +1,10 @@
 package com.stackbilly.compose_tutorial.models
 
 data class Houses(
-    val description: String?,
-    val image:String?,
-    val location: String?,
-    val name:String?,
-    val price:String?,
-    val url:String?
+    val Description: String,
+    val Image:String,
+    val Location: String,
+    val Name:String,
+    val Price:String,
+    val Url:String
 )
